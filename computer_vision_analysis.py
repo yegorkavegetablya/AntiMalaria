c9f1a9c2a86e8ce73385142087a5b81fcf87592d
1ron-I3east <78046681+1ron-I3east@users.noreply.github.com>
2023-12-06 12:33:09 +0300

    Update computer_vision_analysis.py
    Изменение интеграции

--- computer_vision_analysis.py
+++ computer_vision_analysis.py
@@ -1,2 +1,14 @@
 def some_cool_neuronet_function(image_path):
-    return 1
+    model = keras.models.load_model('Malaria Cells.h5')
+    image = cv2.imread('path')
+    image = cv2.resize(image, img_size)  # Приведение изображения к нужному размеру
+    image = image / 255.0  # Масштабирование значений пикселей
+    prediction = model.predict(np.expand_dims(image, axis=0))  # Предсказание для одного изображения
+
+# Получение наиболее вероятного класса
+    predicted_class = np.argmax(prediction)
+    class_name = classes[predicted_class]
+    if class_name == 'Uninfected':
+        return 0
+    else:
+        return 1
